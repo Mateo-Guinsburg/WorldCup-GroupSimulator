307a165932687733a7855758143247bac957c16a
Mateo <mateo.guinsburg@gmail.com>
2026-06-11 14:55:59 +0200

    Fixed .dll issues

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @echo off
-g++ main.cpp team.cpp match.cpp group.cpp tournament.cpp -o worldcup -lws2_32 -std=c++17 -D_WIN32_WINNT=0x0A00
+g++ main.cpp team.cpp match.cpp group.cpp tournament.cpp -o worldcup -lws2_32 -std=c++17 -D_WIN32_WINNT=0x0A00 -static
 echo Compilation ended.
 pause
